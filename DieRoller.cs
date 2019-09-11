2d6e4737689c946c6c1e9059cd24c71dd57dd156
JohntheProgrammer92 <48172932+JohntheProgrammer92@users.noreply.github.com>
2019-09-11 11:12:06 -0400

    Added files from first assignment for C# class
    This die roller tests all overloaded methods in the dieRoller class at once.

---
+++ DieRoller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DieRoller_F2019
+{
+    class DieRoller
+    {
+        public static int Roll()
+        {
+            return StaticRandom.Instance.Next(1, 7);
+        }
+        public static int Roll(int times)
+        {
+            int total = 0;
+            while (times > 0)
+            {
+                total += StaticRandom.Instance.Next(1, 7);
+                times--;
+            }
+            return total;
+        }
+        public static int Roll(int times, int sides)
+        {
+            int total = 0;
+            while (times > 0)
+            {
+                total += StaticRandom.Instance.Next(1, sides + 1);
+                times--;
+            }
+            return total;
+        }
+        public static int Roll(int times, int sides, int target)
+        {
+            int pass = 0;
+            while (times > 0)
+            {
+                int roll = StaticRandom.Instance.Next(1, sides + 1);
+                if (roll >= target)
+                {
+                    pass++;
+                }
+                times--;
+            }
+            return pass;
+        }
+    }
+}
